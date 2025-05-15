2ed2ad0fd7623eaafabca31293d4924ad1484e84
DenisRum05 <147909542+DenisRum05@users.noreply.github.com>
2025-05-15 15:46:37 +0300

    Add files via upload

---
+++ grid.js
@@ -0,0 +1,8 @@
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "10%";
+    }
